49d94fdd6bc36cae8cc4ffa7d132de2772b85bb1
ikalumba <ikalumba221@gmail.com>
2023-10-27 20:18:04 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9596a224-0c13-4710-8410-f02460438028';
+export const MODAL_CLOSE_SEC = 2.5;
